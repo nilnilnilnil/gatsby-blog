e60c954ad7163d0c5e6fd0fba0f12a3152e0cfe4
Jason XJ Bai <jason.bai@petbarn.com.au>
2021-07-01 20:32:34 +1000

    add form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -42,7 +42,8 @@ const MyForm = () => {
                     <form onSubmit={handleOnSubmit}>
                         <input type="email" name="email" placeholder="Your Email">
                             <input type="text" name="name" placeholder="Your Name">
-                                <input type="text" name="message" placeholder="Your Message"><button type="submit">Send</button>
+                                <input type="text" name="message" placeholder="Your Message">
+                                    <button type="submit">Send</button>
                     </form>
                 </div>
             </div>
